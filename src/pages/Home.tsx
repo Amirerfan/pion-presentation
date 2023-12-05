a5b23f2167989c989043c8ca2f545d46af8c7292
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-12-05 18:50:08 +0100

    Home Page Snap Scroll Behavior

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,21 @@
 const Home = () => {
 	return (
-		<div className='bg-red-100'>
-			index page
+		<div className='page w-full'>
+			<section className='page__section hero-section'>
+
+			</section>
+
+			<section className='page__section cost-efficiency-section'>
+
+			</section>
+
+			<section className='page__section real-time-data-section'>
+
+			</section>
+
+			<section className='page__section more-about-pion-section'>
+
+			</section>
 		</div>
 	)
 }

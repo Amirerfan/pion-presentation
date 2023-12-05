fbd22ff51b8400e5d1ff43e779f7f04a054c4a71
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-12-05 20:12:33 +0100

    Cost Efficiency Animated Link

---
+++ src/components/cost-efficiency.tsx
@@ -0,0 +1,21 @@
+const CostEfficiency = () => {
+	return (
+		<div className='hero flex flex-col w-full h-full items-center relative'>
+
+
+			<span className='animate-link absolute bottom-0 translate-y-1/2 hover:-translate-y-4 transition-all group'>
+				<div
+					className='cursor-pointer flex flex-col gap-4 relative w-60 h-60 group-hover:w-[309px] group-hover:h-[309px] transition-all items-center justify-center '>
+					<p
+						className='font-extrabold text-8xl mx-auto text-white group-hover:text-primary-l1 transition-all -mt-5 group-hover:text-[108px]'>{2}</p>
+					<h3
+						className='font-medium text-[28px] leading-[28px] text-white text-center transition-all group-hover:text-[46px] group-hover:leading-[46px] group-hover:text-primary-l1'>Real-Time <br/> Data</h3>
+					</div>
+				<span
+					className='animated-border absolute top-0 w-full h-full border-4 border-dashed rounded-full border-primary-l1 pointer-events-none transition-all duration-300 group-hover:rotate-[10deg]'></span>
+			</span>
+		</div>
+	)
+}
+
+export default CostEfficiency;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
 import Hero from "../components/Hero";
+import CostEfficiency from "../components/cost-efficiency";
 
 const Home = () => {
 	return (
 		<div className='page w-full'>
 
 			<section className='page__section hero-section'>
-				<Hero/>
+				<Hero />
 			</section>
 
 			<section className='page__section cost-efficiency-section'>
-
+				<CostEfficiency />
 			</section>
 
 			<section className='page__section real-time-data-section'>
